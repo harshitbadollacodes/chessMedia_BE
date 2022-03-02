d1f476ec3a46b619f526ee4a189d98a7aa30cd55
harshitbadolla <harshitbadolla@yahoo.com>
2022-03-02 19:24:21 +0530

    added API handlers for comment & edit post

--- router/post.router.js
+++ router/post.router.js
@@ -6,7 +6,7 @@ const { Post } = require("../models/post.model");
 router.route("/")
 .get(async (req, res) => {
     try {
-        const posts = await Post.find({}).sort("-createdAt").populate("user");
+        const posts = await Post.find({}).sort("-createdAt").populate("user comments.user");
         res.json({ message: "success", posts });
     } catch(error) {
         console.log(error);
@@ -31,8 +31,6 @@ router.route("/like/:postId")
     try {
         const { postId } = req.params;
         const userId = req.userId;
-        console.log("userId", userId);
-        console.log("postId", postId);
 
         const post = await Post.findById(postId);
         console.log(post);
@@ -91,9 +89,6 @@ router.route("/new")
         const userId = req.userId;
 
         const { postInput, imageURL } = req.body;
-        
-
-        console.log(typeof imageURL);
 
         const addPost = new Post({ 
             _id: new mongoose.Types.ObjectId(),
@@ -105,13 +100,130 @@ router.route("/new")
         const savePost = await addPost.save();
 
         const newPost = await Post.findById(addPost._id).populate("user");
-        console.log(newPost);
         
         return res.json({ success: true, newPost });
 
     } catch(error) {
         console.log(error);
         res.status(404).json({ 
+            success: false, 
+            message: "check error message", 
+            errorMessage: error.message 
+        });
+    }
+});
+
+router.route("/removePost/:postId")
+.post(async (req, res) => {
+    try {
+        const { postId } = req.params;
+        
+        const post = await Post.findById(postId);
+
+        await post.remove();
+
+        return res.json({
+            success: true,
+            post
+        });
+
+    } catch(error){
+        console.log(error);
+        res.status(400).json({ 
+            success: false, 
+            message: "check error message", 
+            errorMessage: error.message 
+        });
+    }
+});
+
+router.route("/editPost/:postId")
+.post(async (req, res) => {
+    try {
+        const { postId } = req.params;
+        console.log(postId);
+        const { postInput, imageURL } = req.body;
+
+        console.log("imageURL", imageURL);
+
+        const post = await Post.findById(postId);
+        console.log(post);
+        
+        post.postContent = postInput.text;
+        post.imageURL = imageURL;
+
+        await post.save();
+        console.log("save", post);
+
+        return res.json({
+            success: true,
+            post
+        })
+
+    } catch(error) {
+        console.log(error);
+        res.status(400).json({ 
+            success: false, 
+            message: "check error message", 
+            errorMessage: error.message 
+        });
+    }
+
+});
+
+router.route("/comment/:postId")
+.post(async (req, res) => {
+    try {
+        const { userId } = req;
+        const { postId } = req.params;
+        const { inputComment } = req.body;
+
+        const updatePost = await Post.findById(postId);
+        
+        updatePost.comments.push({
+            user: userId,
+            comment: inputComment
+        });
+
+        await updatePost.save();
+
+        const post = await Post.findById(postId).populate("comments.user");
+        
+        return res.json({
+            success: true,
+            post
+        }); 
+    } catch(error){
+        console.log(error);
+        res.status(400).json({ 
+            success: false, 
+            message: "check error message", 
+            errorMessage: error.message 
+        });
+    }
+});
+
+router.route("/removeComment/:postId")
+.delete(async (req, res) => {
+    try {
+        const { postId } = req.params;
+        const { commentId } = req.body;
+
+        const post = await Post.findById(postId).populate("comments.user");
+
+        const findIndex = post.comments.findIndex(comment => String(comment._id) === commentId);
+
+        post.comments[findIndex].remove();
+        await post.save();
+
+        res.json({
+            succes: true,
+            post
+        });
+
+    } catch(error) {
+        console.log(error);
+        res.status(400).json({ 
             success: false, 
             message: "check error message", 
             errorMessage: error.message 
